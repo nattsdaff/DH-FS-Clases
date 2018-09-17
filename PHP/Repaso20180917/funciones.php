448388c6f7eb20a88d2a389d4848a79669ae2b60
digital <dh@dh.com>
2018-09-17 12:33:04 -0300

    DH 17-9

---
+++ PHP/Repaso20180917/funciones.php
@@ -0,0 +1,37 @@
+<?php
+// function validar($datos){ // validar recibe el POST o el GET como si fuera datos
+//   //VOY A BUSCAR LO QUE ESTA MAL, NO LO QUE ESTA BIEN
+//   $errores = []; // hago un array de error y lo lleno con todo lo que esta mal.
+//   if(strlen($datos["nombre"]<3){
+//     $errores["nombre"]="Por favor ingrese un nombre válido";
+//   }
+//   if(strlen($datos["apellido"])<3){
+//     $errores["apellido"]="Por favor ingrese un apellido válido";
+//   }
+//   if(strlen($datos["username"])<5){
+//     $errores["username"]="Por favor ingrese un username válido mayor a 5 caracteres";
+//   }
+//   if(!filter_var($datos["email"],FILTER_VALIDATE_EMAIL)){//validate email es una expresion regular de php, exiten tantas para validar lo que se nos cante.
+//     $errores["email"]="Por favor ingrese un email válido";
+//   }
+//   if($datos["email"]!==$datos["email_confirm"]){
+//     $errores["email"]="Por favor confirme bien su email";
+//   }
+//   if($datos["password"]){
+//     $errores["password"]="Por favor ingrese una contraseña mayor a 8 caracteres";
+//   }
+//   if($datos["password"]!== $datos["contrasea_confirm"]){
+//     $errores["password"]="Las contraseñas no son iguales";
+//   }
+//   if(!$datos["genero"]){
+//     $genero["genero"]="Por favor elija su sexo";
+//   }
+//   if(!$datos["fnac_anio"]-date("Y")>=18){
+//     header("location:http://www.nick.com");
+//   }
+//   if(isset($datos["terminos"])){
+//     $errores["terminos"]="Por favor, llegá al final de la línea";
+//   }
+//   return $errores;
+// }
+ ?>
