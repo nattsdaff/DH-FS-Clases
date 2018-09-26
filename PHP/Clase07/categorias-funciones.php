9a1bb878425f6809451cf82aa75b157ca4735508
nattsdaff <ndaffunchio@gmail.com>
2018-09-26 12:16:42 -0300

    clase

--- PHP/Clase07/categorias-funciones.php
+++ PHP/Clase07/categorias-funciones.php
@@ -1,15 +1,20 @@
 <?php
+
+var_dump($_POST);
+
 function devolverJson($datos){
-  var_dump($datos);
-  $nuevosdatos = json_encode($datos);
-//  var_dump($nuevosdatos);
+  //var_dump($datos);
+  $nuevosdatos[] = json_encode($datos);
+  var_dump($nuevosdatos);
   escribirJson($nuevosdatos);
   return $nuevosdatos;
   }
 
 function escribirJson($datos){
-  if(!file_exists("seleccionados.json")){
-    $openedfile = fopen("seleccionados.json","a+");
+  if(!file_exists("categorias-seleccionados.json")){
+    $openedfile = fopen("categorias-seleccionados.json","a+");
+    var_dump($openedfile);
+    exit;
     $contenidojson = fwrite($openedfile, $datos);
     fclose($openedfile);
   }
